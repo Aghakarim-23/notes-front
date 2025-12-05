3852721c1518cb3bdfe39c660bafb294cbc38ca8
Aghakarim-23 <hemidzade1010@gmail.com>
2025-12-05 17:37:59 +0400

    feat: add interceptors to axios

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,39 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
+  baseURL: "http://localhost:8001/",
   // baseURL: 'https://notes-backend-in1c.onrender.com/',
-  baseURL: 'http://localhost:8001/',
-  
+
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("accessToken");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const status = error.response?.status;
+
+    if (status === 401 || status === 403) {
+      localStorage.removeItem("accessToken");
+      setTimeout(() => {
+        window.location.href = "/login";
+      }, 10);
+    }
+
+    return Promise.reject(error);
+  }
+);
 
 export default api;
