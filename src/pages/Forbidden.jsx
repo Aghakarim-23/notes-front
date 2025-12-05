e00e27a96aa9a3a5928951627e3633e153ef2f82
Aghakarim-23 <hemidzade1010@gmail.com>
2025-12-05 17:56:47 +0400

    style: fix (bg-gradient-to-br) to bg-linear-to-br

--- src/pages/Forbidden.jsx
+++ src/pages/Forbidden.jsx
@@ -1,6 +1,6 @@
 const Forbidden = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-red-100 to-red-200">
+    <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-red-100 to-red-200">
       <div className="bg-white p-16 rounded-2xl shadow-lg text-center max-w-md">
         <h1 className="text-8xl font-bold text-red-600 mb-4">403</h1>
         <h2 className="text-2xl font-semibold mb-2">Forbidden</h2>
